84810ce51e918d00d1182b4cee290820c8c66041
Guyutongxue <guyutongxue@163.com>
2021-10-01 17:04:12 +0800

    init(*): initialize angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+// Copyright (C) 2021 Clavicode Team
+// 
+// This file is part of clavicode-frontend.
+// 
+// clavicode-frontend is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// clavicode-frontend is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with clavicode-frontend.  If not, see <http://www.gnu.org/licenses/>.
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
