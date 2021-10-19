d41676236d7debc0ef231f46dd9b6968d2f5f481
Guyutongxue <guyutongxue@163.com>
2021-10-19 19:09:00 +0800

    style(*): insert license header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,3 +1,20 @@
+// Copyright (C) 2021 Clavicode Team
+// 
+// This file is part of clavicode-frontend.
+// 
+// clavicode-frontend is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// clavicode-frontend is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with clavicode-frontend.  If not, see <http://www.gnu.org/licenses/>.
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
