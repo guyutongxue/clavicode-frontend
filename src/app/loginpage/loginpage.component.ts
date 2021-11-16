41e7ee0554df9a4060ce4e5f52799a67cd4ce45d
YulinZhang255 <58351953+YulinZhang255@users.noreply.github.com>
2021-11-16 09:36:08 +0800

    Add files via upload

---
+++ src/app/loginpage/loginpage.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+
+@Component({
+  selector: 'app-loginpage',
+  templateUrl: './loginpage.component.html',
+  styleUrls: ['./loginpage.component.scss']
+})
+export class LoginpageComponent implements OnInit {
+  validateForm!: FormGroup;
+  checked = true;
+
+
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      console.log('submit', this.validateForm.value);
+    } else {
+      Object.values(this.validateForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      userName: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      remember: [true]
+    });
+  }
+
+}
